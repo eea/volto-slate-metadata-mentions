6006d9070ca8418c508f002032eca087c851b489
Alin Voinea <contact@avoinea.com>
2020-08-05 19:05:19 +0300

    Remove unused uid normalizer

--- src/plugins/Mentions/extensions.js
+++ src/plugins/Mentions/extensions.js
@@ -1,30 +1,8 @@
-import { Transforms } from 'slate';
-import { nanoid } from 'volto-slate/utils';
-
 export const withMentions = (editor) => {
-  const { isInline, normalizeNode } = editor;
+  const { isInline } = editor;
 
   editor.isInline = (element) => {
     return element.type === 'mention' ? true : isInline(element);
-  };
-
-  editor.normalizeNode = (entry) => {
-    const [node, path] = entry;
-    if (node.type === 'mention' && !node.data?.uid) {
-      Transforms.setNodes(
-        editor,
-        {
-          data: {
-            ...node.data,
-            uid: nanoid(5),
-          },
-        },
-        {
-          at: path,
-        },
-      );
-    }
-    return normalizeNode(entry);
   };
 
   return editor;
