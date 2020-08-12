e46936d725629bb705065b8d9799f197e7c26082
Tiberiu Ichim <tiberiu.ichim@gmail.com>
2020-08-12 12:18:06 +0300

    No longer allow text insertion in mention element

--- src/plugins/Mentions/extensions.js
+++ src/plugins/Mentions/extensions.js
@@ -1,10 +1,18 @@
 import { MENTION } from './constants';
+import { isCursorInMention } from './utils';
 
 export const withMention = (editor) => {
-  const { isInline } = editor;
+  const { isInline, insertText } = editor;
 
   editor.isInline = (element) => {
     return element.type === MENTION ? true : isInline(element);
+  };
+
+  editor.insertText = (text) => {
+    if (isCursorInMention(editor)) {
+      return;
+    }
+    return insertText(text);
   };
 
   return editor;
