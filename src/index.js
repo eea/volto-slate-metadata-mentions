a9b712f5c898d2ac9179f00bac23abd20232cbb9
dobri1408 <50819975+dobri1408@users.noreply.github.com>
2023-07-25 16:10:46 +0300

    refactor: Remove volto-widgets-view dependency - refs #254206

--- src/index.js
+++ src/index.js
@@ -1,11 +1,6 @@
 import installPlugins from './plugins';
-import installWidgetsView from '@eeacms/volto-widgets-view';
 
 const applyConfig = (config) => {
-  if (!config.widgets.view) {
-    config = installWidgetsView(config);
-  }
-
   // Restrict slate metadata mentions to Layout only
   config.settings.layoutOnlySlateMetadataMentions =
     config.settings.layoutOnlySlateMetadataMentions ?? false;
