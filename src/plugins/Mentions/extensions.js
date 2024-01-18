fbff16d2d9697d2beaa54497055eca01fa840061
ana-oprea <anaop3212@gmail.com>
2024-01-18 17:15:29 +0200

    fix: check if element exists when rendering extensions -refs #263143

--- src/plugins/Mentions/extensions.js
+++ src/plugins/Mentions/extensions.js
@@ -4,7 +4,7 @@ export const withMention = (editor) => {
   const { isInline } = editor;
 
   editor.isInline = (element) => {
-    return element.type === MENTION ? true : isInline(element);
+    return element && element.type === MENTION ? true : isInline(element);
   };
 
   return editor;
